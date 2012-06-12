e466e65c05b88ff9a372505d5c8883fb0dbe797f
Ben Hamner <ben@benhamner.com>
2012-06-12 14:50:07 -0700

    filled in main metrics function

--- MATLAB/metrics.m
+++ MATLAB/metrics.m
@@ -1,6 +1,74 @@
-function score=metrics()
-% PLACEHOLDER for metrics("AUC",P,C) etc.
+function score=metrics(metric_name, actual, predicted, varargin)
+%METRICS Calculates evaluation metrics commonly used in machine learning
+%
+%   metrics(metric_name, actual, predicted, varargin)
+%   Example: metrics('ap@k', actual, predicted, k)
 %
 %   Author: Ben Hamner (ben@benhamner.com)
 
-score = 0
+switch lower(metric_name)
+    case 'averageprecisionatk'
+        if isempty(varargin)
+            score = averagePrecisionAtK(actual, predicted);
+        else
+            score = averagePrecisionAtK(actual, predicted, varargin{1});
+        end
+    case 'ap@k'
+        if isempty(varargin)
+            score = averagePrecisionAtK(actual, predicted);
+        else
+            score = averagePrecisionAtK(actual, predicted, varargin{1});
+        end    
+    case 'auc'
+        score = auc(actual, predicted);
+    case 'classificationerror'
+        score = classificationError(actual, predicted);
+    case 'logloss'
+        score = logLoss(actual, predicted);
+    case 'quadraticweightedkappa'
+        if isempty(varargin)
+            score = quadraticWeightedKappa(actual, predicted);
+        else
+            score = quadraticWeightedKappa(actual, predicted, ...
+                varargin{1}, varargin{2});
+        end
+    case 'qwk'
+        if isempty(varargin)
+            score = quadraticWeightedKappa(actual, predicted);
+        else
+            score = quadraticWeightedKappa(actual, predicted, ...
+                varargin{1}, varargin{2});
+        end
+    case 'mae'
+        score = mae(actual, predicted);
+    case 'meanabsoluteerror'
+        score = mae(actual, predicted);
+    case 'meanaverageprecisionatk'
+        if isempty(varargin)
+            score = meanAveragePrecisionAtK(actual, predicted);
+        else
+            score = meanAveragePrecisionAtK(actual, predicted, varargin{1});
+        end
+    case 'map@k'
+        if isempty(varargin)
+            score = meanAveragePrecisionAtK(actual, predicted);
+        else
+            score = meanAveragePrecisionAtK(actual, predicted, varargin{1});
+        end   
+    case 'mse'
+        score = mse(actual, predicted);
+    case 'meansquarederror'
+        score = mse(actual, predicted);
+    case 'msle'
+        score = msle(actual, predicted);
+    case 'meansquaredlogerror'
+        score = msle(actual, predicted);
+    case 'rmse'
+        score = rmse(actual, predicted);
+    case 'rootmeansquarederror'
+        score = rmse(actual, predicted);
+    case 'rmsle'
+        score = rmsle(actual, predicted);
+    case 'rootmeansquaredlogerror'
+        score = rmsle(actual, predicted);
+end
