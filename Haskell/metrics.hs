0ac4c65753b47ea638831040fe96ec850aa24311
Ben Hamner <ben@benhamner.com>
2012-06-13 00:16:05 -0700

    Started creating metrics.hs

---
+++ Haskell/metrics.hs
@@ -0,0 +1,28 @@
+import Math.Statistics (mean)
+
+ae :: Num a => a -> a -> a
+ae x y = abs (x-y)
+
+se :: Num a => a -> a -> a
+se x y = (x-y)^2
+
+sle :: Double -> Double -> Double
+sle x y = ((log x+1) - (log y+1))^2
+
+meanZipWith :: (a -> b -> Double) -> [a] -> [b] -> Double
+meanZipWith = ((mean .) .) . zipWith
+
+mae :: [Double] -> [Double] -> Double
+mae = meanZipWith ae
+
+mse :: [Double] -> [Double] -> Double
+mse = meanZipWith se
+
+msle :: [Double] -> [Double] -> Double
+msle = meanZipWith sle
+
+rmse :: [Double] -> [Double] -> Double
+rmse = (sqrt .) . mse
+
+rmsle :: [Double] -> [Double] -> Double
+rmsle = (sqrt .) . msle
