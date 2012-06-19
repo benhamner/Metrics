405af92d06804590e64b7bd1a9ff9c8e2dfd9dbf
Ben Hamner <ben@benhamner.com>
2012-06-18 22:37:24 -0700

    Metrics now works with Octave

--- MATLAB/metrics/auc.m
+++ MATLAB/metrics/auc.m
@@ -16,6 +16,29 @@ function auc = auc(category,posterior)
 % David Hand and Robert Till
 % http://www.springerlink.com/content/nn141j42838n7u21/fulltext.pdf
 
-r = tiedrank(posterior);
+if exist('tiedrank')
+    r = tiedrank(posterior);
+else
+    r = tiedrank_metrics(posterior);
+end
 auc = (sum(r(category==1)) - sum(category==1)*(sum(category==1)+1)/2) / ...
     ( sum(category<1)*sum(category==1));
+
+function r = tiedrank_metrics(x)
+
+[~,I] = sort(x);
+
+r = 0*x;
+cur_val = x(I(1));
+last_pos = 1;
+
+for i=1:length(I)
+    if cur_val ~= x(I(i))
+        r(I(last_pos:i-1)) = (last_pos+i-1)/2;
+        last_pos = i;
+        cur_val = x(I(i));
+    end
+    if i==length(I)
+        r(I(last_pos:i)) = (last_pos+i)/2;
+    end
+end
