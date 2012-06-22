959dbfe0b3033791e49ce0973ac13b83d8d4e5f0
Ben Hamner <ben@benhamner.com>
2012-06-22 02:03:11 -0400

    Python 0.1.2 release

--- Python/setup.py
+++ Python/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='ml_metrics',
-      version='0.1.1',
+      version='0.1.2',
       description='Machine Learning Evaluation Metrics',
       author = 'Ben Hamner',
       author_email = 'ben@benhamner.com',
