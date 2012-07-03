15255afc98531e3f1e09776d8c863fa3289e1e67
Ben Hamner <ben@benhamner.com>
2012-07-03 16:13:05 -0400

    Improved Python levenshtein implementation

--- Python/ml_metrics/edit_distance.py
+++ Python/ml_metrics/edit_distance.py
@@ -1,19 +1,17 @@
-import numpy as np
 
-def levenshtein(str1, str2):
-    m = len(str1)
-    n = len(str2)
-    distance_matrix = np.zeros((m+1, n+1))
-    distance_matrix[range(m+1),0] = range(m+1)
-    distance_matrix[0,range(n+1)] = range(n+1)
+def levenshtein(s1, s2):
+    if len(s1) < len(s2):
+        return levenshtein(s2, s1)
+    if not s2:
+        return len(s1)
 
-    for i in range(1,m+1):
-        for j in range(1,n+1):
-            if str1[i-1]==str2[j-1]:
-                distance_matrix[i,j] = distance_matrix[i-1,j-1]
-            else:
-                distance_matrix[i,j] = min(distance_matrix[i-1,j]+1, 
-                                           distance_matrix[i,j-1]+1,
-                                           distance_matrix[i-1,j-1]+1)
-
-    return distance_matrix[m,n]
+    previous_row = range(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        current_row = [i+1]
+        for j, c2 in enumerate(s2):
+            insertions = previous_row[j+1] + 1
+            deletions = current_row[j] + 1
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+    return current_row[-1]
