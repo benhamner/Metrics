4dc2c55c997cbcfc3448e1cd1647560b04e206c2
Ben Hamner <ben@benhamner.com>
2012-08-11 07:00:34 -0400

    Added capped_binomial_deviance

--- Python/ml_metrics/__init__.py
+++ Python/ml_metrics/__init__.py
@@ -4,3 +4,4 @@ from auc import auc
 from average_precision import apk, mapk
 from edit_distance import levenshtein
 from custom.kdd_average_precision import kdd_apk, kdd_mapk
+from custom.binomial_deviance import capped_log10_likelihood, capped_binomial_deviance
---
+++ Python/ml_metrics/custom/binomial_deviance.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+def capped_log10_likelihood(actual, predicted):
+    """
+    Computes the capped log10 likelihood, predictions in (0.01, 0.99).
+
+    This function computes the log likelihood between two numbers,
+    or for element between a pair of lists or numpy arrays.
+
+    Parameters
+    ----------
+    actual : int, float, list of numbers, numpy array
+             The ground truth value
+    predicted : same type as actual
+                The predicted value
+
+    Returns
+    -------
+    score : double or list of doubles
+            The log likelihood error between actual and predicted
+
+    """
+    actual = np.array(actual)
+    predicted = np.array(predicted)
+    predicted[predicted<0.01]=0.01
+    predicted[predicted>0.99]=0.99
+
+    score = -(actual*np.log10(predicted)+(1-actual)*np.log10(1-predicted))
+    np.seterr(divide=err['divide'], over=err['over'],
+              under=err['under'], invalid=err['invalid'])
+    if type(score)==np.ndarray:
+        score[np.isnan(score)] = 0
+    else:
+        if np.isnan(score):
+            score = 0
+    return score
+
+def capped_binomial_deviance(actual, predicted):
+    """
+    Computes the capped binomial deviance.
+
+    This function computes the log loss between two lists
+    of numbers.
+
+    Parameters
+    ----------
+    actual : list of numbers, numpy array
+             The ground truth value
+    predicted : same type as actual
+                The predicted value
+
+    Returns
+    -------
+    score : double
+            The log loss between actual and predicted
+
+    """
+    return np.mean(capped_log10_likelihood(actual, predicted))
