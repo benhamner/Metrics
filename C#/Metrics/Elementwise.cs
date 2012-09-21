ea77c96249418938edc2678c73bb6b4191107bee
Ben Hamner <ben@benhamner.com>
2012-09-21 15:44:04 -0700

    Added NUnit

--- C#/Metrics/Elementwise.cs
+++ C#/Metrics/Elementwise.cs
@@ -6,13 +6,21 @@ using Koalas;
 
 namespace Metrics
 {
-    class Elementwise : Metric {
+    public abstract class MeanError : Metric {
         public override bool IsMax { get { throw new NotImplementedException(); } }
         public override string Name { get { throw new NotImplementedException(); } }
         public override string Description { get { throw new NotImplementedException(); } }
 
-        public override double Score(DataFrame actual, DataFrame predicted) {
-            throw new NotImplementedException();
+        public override double Score(DataFrame solution, DataFrame submission) {
+            return solution.GetSeriesByType<Double>().Zip(submission.GetSeriesByType<Double>(), CellError).ToSeries().Mean();
+        }
+
+        public abstract double CellError(double actual, double predicted);
+    }
+
+    public class MeanSquaredError : MeanError {
+        public override double CellError(double actual, double predicted) {
+            return Math.Pow(actual - predicted, 2.0);
         }
     }
 }
--- C#/Metrics/MetricsTests/MseTest.cs
+++ C#/Metrics/MetricsTests/MseTest.cs
@@ -1,11 +1,26 @@
-﻿using System;
+﻿using Koalas;
+using Metrics;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+
 
 namespace MetricsTests
 {
     public class MseTest
     {
+        [Test]
+        public void MseTests() {
+            var solution = DataFrame.FromCsvData(@"1.0
+2.0
+3");
+            var submission = DataFrame.FromCsvData(@"2.0
+3.0
+4");
+            var scoreKeeper = new MeanSquaredError();
+            Assert.AreEqual(1.0, scoreKeeper.Score(solution, submission));
+        }
     }
 }
