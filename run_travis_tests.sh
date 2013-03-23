77420879668ebfc44bab8ff8bcdd959acd16de9f
Ben Hamner <ben@benhamner.com>
2013-03-23 14:29:57 -0500

    another attempt to get installations for travis running silently

--- run_travis_tests.sh
+++ run_travis_tests.sh
@@ -1,13 +1,13 @@
 # octave tests
 cd MATLAB
-yes | sudo apt-add-repository ppa:octave/stable
+yes | sudo apt-add-repository ppa:octave/stable > add_octave_repo_output.txt
 sudo apt-get -qq update
-sudo apt-get -qq install octave
+sudo apt-get -qq install octave > install_octave_output.txt
 octave --eval setup.m
 ./run_octave_tests.sh
 cd ..
 
 # python tests
 cd Python
-python setup.py -q install
+python setup.py -q install > install_python_package_output.txt
 ./run_python_tests.sh
