2b80e389be18e4d1fd447309fdc2de174dbcd854
Ben Hamner <ben@benhamner.com>
2013-03-23 04:29:54 -0500

    updated setup to install requirements

--- Python/setup.py
+++ Python/setup.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python2.7
 
-from distutils.core import setup
+from setuptools import setup
+
+requirements = [x.strip() for x in open("requirements.txt").readlines()]
 
 setup(name='ml_metrics',
       version='0.1.2',
       description='Machine Learning Evaluation Metrics',
       author = 'Ben Hamner',
       author_email = 'ben@benhamner.com',
-      packages = ['ml_metrics', 'ml_metrics.custom'])
+      packages = ['ml_metrics', 'ml_metrics.custom'],
+      install_requires = requirements)
