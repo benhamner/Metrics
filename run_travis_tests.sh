12b980bc07830fb3bf8966b4641a13bc4a64ce89
Ben Hamner <ben@benhamner.com>
2013-03-23 14:18:55 -0500

    installations should be running silently now

--- run_travis_tests.sh
+++ run_travis_tests.sh
@@ -1,13 +1,13 @@
 # octave tests
 cd MATLAB
 yes | sudo apt-add-repository ppa:octave/stable
-sudo apt-get update
-sudo apt-get install -qq octave
+sudo apt-get -qq update
+sudo apt-get -qq install octave
 octave --eval setup.m
 ./run_octave_tests.sh
 cd ..
 
 # python tests
 cd Python
-python setup.py install
+python setup.py -q install
 ./run_python_tests.sh
