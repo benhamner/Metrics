91d3f6e65353567065163f27433e6cbffe6e01a0
Ben Hamner <ben@benhamner.com>
2013-03-23 14:54:33 -0500

    travis octave tests should be running more silently

--- run_travis_tests.sh
+++ run_travis_tests.sh
@@ -1,13 +1,13 @@
 # octave tests
 cd MATLAB
-yes | sudo apt-add-repository ppa:octave/stable &> add_octave_repo_output.txt
+yes | sudo apt-add-repository ppa:octave/stable
 sudo apt-get -qq update
-sudo apt-get -qq install octave &> install_octave_output.txt
+sudo apt-get -qq install octave > /dev/null
 octave -q --eval setup.m
 ./run_octave_tests.sh
 cd ..
 
 # python tests
 cd Python
-python setup.py -q install &> install_python_package_output.txt
+python setup.py -q install > /dev/null
 ./run_python_tests.sh
