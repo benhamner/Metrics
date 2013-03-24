f15bd4f968fbdfaff928b625fa938d4ad6b07583
Ben Hamner <ben@benhamner.com>
2013-03-24 17:56:37 -0500

    trying to get R tests working on travis

--- run_travis_tests.sh
+++ run_travis_tests.sh
@@ -12,8 +12,8 @@ python setup.py -q install > /dev/null 2>&1
 cd ..
 
 # R tests
-cd R
 sudo apt-get install r-base
-R CMD INSTALL -l . metrics
+R CMD INSTALL -l R metrics
+cd R
 sh run_r_tests.sh
 cd ..
