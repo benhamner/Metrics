5237f06899b7c8f30742151be496bc54cf10edb1
Ben Hamner <ben@benhamner.com>
2013-03-24 12:52:39 -0500

    trying to only show test information on terminal output for travis

--- run_travis_tests.sh
+++ run_travis_tests.sh
@@ -1,6 +1,6 @@
 # octave tests
 cd MATLAB
-yes | sudo apt-add-repository ppa:octave/stable
+sudo apt-add-repository -q -y ppa:octave/stable
 sudo apt-get -qq update
 sudo apt-get -qq install octave > /dev/null
 octave -q --eval setup.m
@@ -9,5 +9,5 @@ cd ..
 
 # python tests
 cd Python
-python setup.py -q install > /dev/null
+python setup.py -q install > /dev/null 2>&1
 ./run_python_tests.sh
