9f1b6bff57fe7dcb49a1c544233e337d8743c541
Dan Blanchard <dblanchard@ets.org>
2013-04-02 13:21:14 -0400

    Added support for Python 3

--- Python/setup.py
+++ Python/setup.py
@@ -2,7 +2,12 @@
 
 from setuptools import setup
 
-requirements = [x.strip() for x in open("requirements.txt").readlines()]
+requirements = [x.strip() for x in open("requirements.txt")]
+
+# Automatically run 2to3 for Python 3 support
+extra = {}
+if sys.version_info >= (3,):
+    extra['use_2to3'] = True
 
 setup(name='ml_metrics',
       version='0.1.2',
@@ -10,4 +15,5 @@ setup(name='ml_metrics',
       author = 'Ben Hamner',
       author_email = 'ben@benhamner.com',
       packages = ['ml_metrics', 'ml_metrics.custom'],
-      install_requires = requirements)
+      install_requires = requirements,
+      **extra)
