dd476e08897b408a7120d5f091a686657aee77ec
Adam Hoelscher, ASA, MAAA <adam.hoelscher@gmail.com>
2018-06-28 09:37:11 -0700

    Refactor auc to avoid multiplication
    Multiplying n_pos and n_neg can result in integer overflow. A little algebraic manipulation can avoid the multiplication, removing the possibility of overflow.

--- R/R/metrics.r
+++ R/R/metrics.r
@@ -123,7 +123,7 @@ auc <- function(actual, predicted)
     r <- rank(predicted)
     n_pos <- sum(actual==1)
     n_neg <- length(actual) - n_pos
-    auc <- (sum(r[actual==1]) - n_pos*(n_pos+1)/2) / (n_pos*n_neg)
+    auc <- sum(r[actual==1])/n_pos/n_neg - (n_pos+1)/2/n_neg
     auc
 }
 
